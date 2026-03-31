2debc97a4de076e9f5408424578d20854bb09fbf
Samuel Benjamim Mendes dos Santos <dev.samuel.mendes@gmail.com>
2026-03-31 09:55:10 -0300

    Primeiro commit

---
+++ src/main/java/com/samu/dev/arcflow/ArcflowApplication.java
@@ -0,0 +1,13 @@
+package com.samu.dev.arcflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArcflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samu/dev/arcflow/ArcflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samu.dev.arcflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
